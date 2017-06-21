0c479be67d95dfff54e67c827bc92e3c1c5c8e97
Kuba Turek <ja@kubaturek.pl>
2017-06-21 09:58:20 +0200

    Manually register bundle extension

--- src/Req2CmdBundle.php
+++ src/Req2CmdBundle.php
@@ -3,8 +3,22 @@ declare(strict_types=1);
 
 namespace Eps\Req2CmdBundle;
 
+use Eps\Req2CmdBundle\DependencyInjection\Req2CmdExtension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 final class Req2CmdBundle extends Bundle
 {
+    /**
+     * @codeCoverageIgnore
+     */
+    public function getContainerExtension(): Extension
+    {
+        if ($this->extension === null) {
+            $this->extension = new Req2CmdExtension();
+        }
+
+        return $this->extension;
+    }
+
 }
